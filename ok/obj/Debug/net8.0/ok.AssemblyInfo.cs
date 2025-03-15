c34abdf1dd31c91f723340ef977fd8c6ee9effd2
tongtuan-vip <dtc225201398@ictu.edu.vn>
2025-03-15 14:36:30 +0700

    dang viet dong 7

--- ok/obj/Debug/net8.0/ok.AssemblyInfo.cs
+++ ok/obj/Debug/net8.0/ok.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ok")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf138a5eaaa328465dcd45e8f59596f442c9e8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8083aca8441f588578f62580680fd681a6c9c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ok")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ok")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
